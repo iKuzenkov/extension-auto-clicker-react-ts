f00616b6b949773ccc782e1e5f0ff46b416adc9b
Igor <greendaycom1@gmail.com>
2026-04-18 17:44:49 +0300

    refactor: rename file

--- src/hooks/input-validation/logic/delay-in-milliseconds.ts
+++ src/hooks/input-validation/logic/delay-in-milliseconds.ts
@@ -1,6 +1,6 @@
 function delayInSeconds(h: number, m: number, s: number): number | null {
   if (h > 10000 || m > 59 || s > 59) return null;
-  return h * 3600 + m * 60 + s;
+  return (h * 3600 + m * 60 + s) * 1000;
 }
 
 export default delayInSeconds;
