85d0c92d72a2b9b25c012945d4a13518c4c39999
Igor <greendaycom1@gmail.com>
2026-04-18 17:52:34 +0300

    add: change state for stop clicking

--- src/components/ButtonStop/ButtonStop.tsx
+++ src/components/ButtonStop/ButtonStop.tsx
@@ -1,13 +1,18 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Button from '../ReusableComponents/Button/Button';
 import type { Theme } from '../../types/global-state-types/GlobalTypes';
-import type { RootState } from '../../store/store';
+import type { AppDispatch, RootState } from '../../store/store';
 import './ButtonStop.scss';
+import { timerState } from '../../features/logicSlice';
 
 function ButtonStop() {
   const state: Theme = useSelector((state: RootState): Theme => state.ui.theme);
+  const dispatch = useDispatch<AppDispatch>();
 
-  const onStopClick = () => {};
+  const onStopClick = () => {
+    dispatch(timerState(false));
+    console.log('onStopClick');
+  };
 
   return (
     <>
