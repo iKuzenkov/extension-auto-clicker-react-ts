9c104d55d7007c7f6b6172751c2e88997115df15
Igor <greendaycom1@gmail.com>
2026-04-21 17:50:20 +0300

    fix: empty data bug

--- src/hooks/input-validation/logic/delay-in-milliseconds.ts
+++ src/hooks/input-validation/logic/delay-in-milliseconds.ts
@@ -2,6 +2,7 @@ import type { Delay } from '../../../types/global-state-types/GlobalTypes';
 
 function delayInMilliseconds(h: number, m: number, s: number): Delay {
   if (h > 10000 || m > 59 || s > 59) return null;
+  if (h <= 0 && m <= 0 && s <= 0) return null;
   return (h * 3600 + m * 60 + s) * 1000;
 }
 
