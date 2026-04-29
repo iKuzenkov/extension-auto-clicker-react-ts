fc65df12d5f6e0a545ca72de25cf423dd7579299
Igor <greendaycom1@gmail.com>
2026-04-30 01:33:30 +0300

    feat: test Fetch API

--- src/components/MoreInfo/data/dataInfo.ts
+++
@@ -1,9 +0,0 @@
-import getData from '../../../api/get-data-api/getData';
-import type { DataInfo } from './TypesData';
-
-async function dataInfo(): Promise<DataInfo> {
-  return await getData(
-    'https://raw.githubusercontent.com/iKuzenkov/portfolio-api-data/refs/heads/main/auto-clicker/how-to-use.json'
-  );
-}
-export default dataInfo;
