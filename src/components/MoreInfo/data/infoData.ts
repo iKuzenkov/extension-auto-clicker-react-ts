5aefde922b5c0334e5f62484e47577d4b268194f
Igor <greendaycom1@gmail.com>
2026-06-26 00:26:30 +0300

    refactor: modules

--- src/components/MoreInfo/data/infoData.ts
+++
@@ -1,9 +0,0 @@
-import getFetchData from '../../../api/get-fetch-data-api/getFetchData';
-import type { DataInfo } from './TypesData';
-
-async function infoData(): Promise<DataInfo> {
-  return await getFetchData(
-    'https://raw.githubusercontent.com/iKuzenkov/portfolio-api-data/refs/heads/main/auto-clicker/how-to-use.json'
-  );
-}
-export default infoData;
